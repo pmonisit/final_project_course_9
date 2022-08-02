593ca5d3d9e350bb78532684087fa944383c232a
pmonisit <41044547+pmonisit@users.noreply.github.com>
2022-08-02 12:42:45 +0800

    Update admin.py
    Update admin.py

--- final_template/onlinecourse/admin.py
+++ final_template/onlinecourse/admin.py
@@ -4,15 +4,13 @@ from .models import Course, Lesson, Instructor, Learner
 from .models import Question, Choice
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
-class ChoiceInline(admin.TabularInline):
-    model = Choice
-    extra = 3
-
-
-class QuestionAdmin(admin.ModelAdmin):
+class QuestionInline(admin.StackedInline):
     model = Question
-    inlines = [ChoiceInline]
+    extra = 4
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 4
 
 class LessonInline(admin.StackedInline):
     model = Lesson
@@ -21,19 +19,25 @@ class LessonInline(admin.StackedInline):
 
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
-    inlines = [LessonInline]
+    inlines = [LessonInline, QuestionInline]
     list_display = ('name', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
 
-
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ['course', 'text',]
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ['text', 'is_correct']
 
 # <HINT> Register Question and Choice models here
-admin.site.register(Question, QuestionAdmin)
+
+admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
-admin.site.register(Course, CourseAdmin)
